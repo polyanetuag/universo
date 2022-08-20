f375189aea51defeb603769d54dbe721a752885e
Polyane Tuag <pollyportugues@hotmail.com>
2022-08-20 10:15:51 -0300

    feat: create header structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function App() {
+  return (
+    <header className="content-App">
+      {/* <span>Logo</span> */}
+      <p>Sobre</p>
+      <p>Planetas</p>
+      <p>Curiosidades</p>
+    </header>
+  );
+}
+
+export default App;
