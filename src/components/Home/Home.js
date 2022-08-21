0522ee576ce3e1d608bce4f7816c4c43648b35fe
Polyane Tuag <pollyportugues@hotmail.com>
2022-08-21 11:27:39 -0300

    feat: Home structure

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <section>
+        <h1> Bem vindo ao Universo</h1>
+        <p>O universo é a extensão tridimensional sem limites em que objetos e eventos têm posição relativa - <strong>Vá além</strong></p>
+        <button>Explore</button>
+        <p>"Em algum lugar, algo incrível está esperando para ser descoberto." - Carl sagan</p>
+    </section>
+  );
+}
+
+export default Home;
