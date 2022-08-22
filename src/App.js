336af5b1ffe3316cb3c74da300d4310b044976c0
Polyane Tuag <pollyportugues@hotmail.com>
2022-08-22 19:05:32 -0300

    fix: remove header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 
 import './App.css';
-import Header from './components/Header/Header.js';
+// import Header from './components/Header/Header.js';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       <Home />
-      {/* <p>Footer</p> */}
-      {/* <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Explore
-        </a>
-      </header> */}
+      <p className=''>"Em algum lugar, algo incrível está esperando para ser descoberto." - Carl sagan</p>
     </div>
   );
 }
