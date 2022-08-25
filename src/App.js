b9c3dedf47c8353aa75f2b482df0549a85ee5562
Polyane Tuag <pollyportugues@hotmail.com>
2022-08-25 18:58:40 -0300

    fix: add section in app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
-import './App.css';
+import "./App.css";
 // import Header from './components/Header/Header.js';
-import Home from './components/Home/Home';
+// import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      <Home />
-      <p className=''>"Em algum lugar, algo incrível está esperando para ser descoberto." - Carl sagan</p>
+      {/* <Home /> */}
+      <section>
+        <h1> Bem vindo ao Universo</h1>
+
+        <span>
+          Explore a extensão tridimensional sem limites em que objetos e eventos
+          têm posição relativa
+        </span>
+        <button>Explore</button>
+
+        <p className="">
+          "Em algum lugar, algo incrível está esperando para ser descoberto."
+        </p>
+        <p>Carl Sagan</p>
+      </section>
     </div>
   );
 }
