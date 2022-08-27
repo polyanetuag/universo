c8a771d17d449092403ea2b1f3b0071bb2afa961
Polyane Tuag <pollyportugues@hotmail.com>
2022-08-27 11:18:07 -0300

    fix: button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
+
 // import Header from './components/Header/Header.js';
 // import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      {/* <Home /> */}
       <section>
         <h1> Bem vindo ao Universo</h1>
 
-        <span>
-          Explore a extensão tridimensional sem limites 
-        </span>
-        <button>Explore</button>
-    
+        <span>Explore a extensão tridimensional sem limites</span>
+        {/* // botão para página de Page */}
+        <button>
+          <a href="/components/Page">
+            <span>Explore</span>
+          </a>
+        </button>
+
         <p className="">
           "Em algum lugar, algo incrível está esperando para ser descoberto."
         </p>
