634233a7d3dd0bd8c950e384239aa2da8664533d
Polyane Tuag <pollyportugues@hotmail.com>
2022-08-28 12:04:27 -0300

    fix: nav

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // import Header from './components/Header/Header.js';
 // import Home from './components/Home/Home';
+import Page from "./components/Page/Page";
 
 function App() {
   return (
-    <div className="App">
-      <section>
-        <h1> Bem vindo ao Universo</h1>
+    <Router>
+      <div className="App">
+        <section>
+          <h1> Bem vindo ao Universo</h1>
+          <span>Explore a extensão tridimensional sem limites</span>
 
-        <span>Explore a extensão tridimensional sem limites</span>
-        {/* // botão para página de Page */}
-        <button>
-          <a href="/components/Page">
-            <span>Explore</span>
-          </a>
-        </button>
+          <a href={<Page />}>Explore</a>
+          {/* <Routes>
+            <Route path="/page" element={<Page />} />
+          </Routes> */}
 
-        <p className="">
-          "Em algum lugar, algo incrível está esperando para ser descoberto."
-        </p>
-        <p>Carl Sagan</p>
-      </section>
-    </div>
+          <p className="">
+            "Em algum lugar, algo incrível está esperando para ser descoberto."
+          </p>
+          <p>Carl Sagan</p>
+        </section>
+      </div>
+    </Router>
   );
 }
 
